d13e522d89f883a8449b560760c20dd17a00b866
AADevelops <amunahmad12@gmail.com>
2025-04-20 20:47:09 -0400

    Small fix.

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,7 +1,7 @@
 function ComingSoon() {
   return (
     <div className="min-h-screen bg-[#111111] font-mono flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-semibold tracking-tight">
+      <h1 className="text-5xl font-semibold tracking-tight">
         <span className="rounded-lg bg-red-900/30 text-red-400 px-2 py-1">
           COMING SOON!
         </span>
