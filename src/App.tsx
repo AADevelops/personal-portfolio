382f3e88aacc53825607a2e86669075e6a1ca71c
AADevelops <amunahmad12@gmail.com>
2025-04-22 07:21:18 -0400

    Completed page routing for each website section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import "./index.css";
 import "./App.css";
-import ComingSoon from "./pages/ComingSoon.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "@/pages/Home.tsx";
+import About from "@/pages/About.tsx";
+import Projects from "@/pages/Projects.tsx";
+import Experience from "@/pages/Experience.tsx";
+import Contact from "@/pages/Contact.tsx";
 
 function App() {
   return (
-    <>
-      <ComingSoon />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
