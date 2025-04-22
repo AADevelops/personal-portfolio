2511ce783e15b8cdf81e32ffe6afb7f540787f50
AADevelops <amunahmad12@gmail.com>
2025-04-22 07:25:33 -0400

    Added remaining pages - not completed yet.

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/Navbar.tsx";
+import Tag from "@/components/Tag.tsx";
+import Footer from "@/components/Footer.tsx";
+
+function Contact() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="bg-[#111111] font-mono text-white flex flex-col flex-1 items-center justify-center space-y-6">
+        <h1 className="sm:text-4xl font-semibold">
+          <Tag label="CONTACT PAGE - COMING SOON" preset="red" />
+        </h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Contact;
